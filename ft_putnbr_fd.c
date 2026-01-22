79399a6e787c807170221ac25c5b0f4c16885147
Dylan Taylor Barca <dtaylor-@student.42madrid.com>
2026-01-22 12:19:08 +0100

    Fixed putnbr without leaks

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: dtaylor- <dtaylor-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 19:22:40 by dtaylor-          #+#    #+#             */
-/*   Updated: 2026/01/21 18:01:44 by dtaylor-         ###   ########.fr       */
+/*   Updated: 2026/01/22 12:18:33 by dtaylor-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,30 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*x;
+	unsigned int	mod;
+	int				count;
+	char			num[11];
 
-	x = ft_itoa(n);
-	while (*x)
+	if (n == 0)
 	{
-		write(fd, &*x, sizeof(char));
-		x++;
+		write(fd, "0", 1);
+		return ;
 	}
+	if (n < 0)
+	{
+		write(fd, "-", 1);
+		mod = -n;
+	}
+	else
+		mod = n;
+	count = 0;
+	while (mod > 0)
+	{
+		num[count++] = (mod % 10) + '0';
+		mod /= 10;
+	}
+	while (count -- > 0)
+		write(fd, &num[count], 1);
 }
 
 /*int	main(void)
