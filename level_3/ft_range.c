cd01238711709f6d86c5c246d4d30ed23e8acc97
Dylan Taylor Barca <dtaylor-@student.42madrid.com>
2026-03-20 17:31:24 +0100

    ft_range.c

---
+++ level_3/ft_range.c
@@ -0,0 +1,60 @@
+/*
+Assignment name  : ft_range
+Expected files   : ft_range.c
+Allowed functions: malloc
+--------------------------------------------------------------------------------
+
+Write the following function:
+
+int     *ft_range(int start, int end);
+
+It must allocate (with malloc()) an array of integers, fill it with consecutive
+values that begin at start and end at end (Including start and end !), then
+return a pointer to the first value of the array.
+
+Examples:
+
+- With (1, 3) you will return an array containing 1, 2 and 3.
+- With (-1, 2) you will return an array containing -1, 0, 1 and 2.
+- With (0, 0) you will return an array containing 0.
+- With (0, -3) you will return an array containing 0, -1, -2 and -3.
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int     *ft_range(int start, int end)
+{
+	int	range = 0;
+	int	*nbrs;
+	int	i = 0;
+
+	if (end >= start)
+		range = end - start;
+	else
+		range = start - end;
+	nbrs = malloc((range + 1) * sizeof(int));
+	if (end >= start)
+	{
+		while (start <= end)
+			nbrs[i ++] = start ++;
+	}
+	else
+	{
+		while (end <= start)
+			nbrs[i ++] = start --;
+	}
+	return (nbrs);
+}
+
+int	main(void)
+{
+	int				start = 2;
+	int				end = -100;
+	int				*nbrs = ft_range(start, end);
+	unsigned long	i = 0;
+	unsigned long size = (start > end) ? (start - end) : (end - start);
+
+	while (i <= size)
+		printf("%d -> ", nbrs[i++]);
+}
