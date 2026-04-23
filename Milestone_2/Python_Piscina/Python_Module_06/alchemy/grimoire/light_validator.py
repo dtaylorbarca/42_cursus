aa84f6b2d22db6b108b608a2560f858b39922259
Dylan Taylor Barca <dtaylorbarca@gmail.com>
2026-04-23 12:55:44 +0200

    python

--- Milestone_2/Python_Piscina/Python_Module_06/alchemy/grimoire/light_validator.py
+++ Milestone_2/Python_Piscina/Python_Module_06/alchemy/grimoire/light_validator.py
@@ -1,5 +1,7 @@
+from .light_spellbook import light_spell_allowed_ingredients
+
+
 def validate_ingredients(ingredients: str) -> str:
-    from .light_spellbook import light_spell_allowed_ingredients
     allowed = light_spell_allowed_ingredients()
     valid = any(i in ingredients.lower() for i in allowed)
     keyword = 'VALID' if valid else 'INVALID'
