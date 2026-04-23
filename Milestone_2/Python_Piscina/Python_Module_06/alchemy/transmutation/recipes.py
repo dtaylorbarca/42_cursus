cbb7876a524aba53f711b3930ec11a6f0d79d76c
Dylan Taylor Barca <dtaylorbarca@gmail.com>
2026-04-23 12:15:49 +0200

    python

--- Milestone_2/Python_Piscina/Python_Module_06/alchemy/transmutation/recipes.py
+++ Milestone_2/Python_Piscina/Python_Module_06/alchemy/transmutation/recipes.py
@@ -2,5 +2,8 @@ from elements import create_fire
 from alchemy.elements import create_air
 from ..potions import strength_potion
 
+
 def lead_to_gold() -> str:
-    return f"Recipe transmuting Lead to Gold: brew '{create_air}' and '{strength_potion}' mixed with '{create_fire}'"
+    return ("Recipe transmuting Lead to Gold: "
+            f"brew '{create_air}' and '{strength_potion}' mixed "
+            f"with '{create_fire}'")
--- Milestone_2/Python_Piscina/Python_Module_06/ft_alembic_4.py
+++ Milestone_2/Python_Piscina/Python_Module_06/ft_alembic_4.py
@@ -1,8 +1,14 @@
 import alchemy
 
-print("=== Albemic 4 ===")
-print("Accessing the alchemy module using 'import alchemy'")
-print(f"Testing create_air: {alchemy.create_air()}")
-print("Now show that not all functions can be reached")
-print("This will raise an exception!")
-print(f"{alchemy.create_earth()}")
+
+def main() -> None:
+    print("=== Albemic 4 ===")
+    print("Accessing the alchemy module using 'import alchemy'")
+    print(f"Testing create_air: {alchemy.create_air()}")
+    print("Now show that not all functions can be reached")
+    print("This will raise an exception!")
+    print(f"{alchemy.create_earth()}")
+
+
+if __name__ == "__main__":
+    main()
