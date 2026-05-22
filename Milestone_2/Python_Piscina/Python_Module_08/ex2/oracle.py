8393d8dcbb2974b0ab42d7ef4f7fce0b230241fc
Dylan Taylor Barca <dtaylor-@student.42madrid.com>
2026-05-22 17:36:45 +0200

    Updating

--- Milestone_2/Python_Piscina/Python_Module_08/ex2/oracle.py
+++ Milestone_2/Python_Piscina/Python_Module_08/ex2/oracle.py
@@ -3,6 +3,47 @@ import sys
 from dotenv import load_dotenv
 
 
+def check_harcoded_secrets(file) -> bool:
+    secret_keys = ["DATABASE_URL", "API_KEY"]
+
+    with open(file, 'r') as file:
+        lines = file.readlines()
+
+    for line in lines:
+        clean_line = line.strip()
+        for key in secret_keys:
+            if clean_line.startswith(key) and "=" in clean_line:
+                value = clean_line.split("=", 1)[1].strip()
+                if (("os.getenv" not in value) and ("os.environ" not in value)
+                        and value != ""):
+                    if ((value.startswith('"') and value.endswith('"')) or
+                            (value.startswith("'") and value.endswith("'"))):
+                        return False
+    return True
+
+
+def env_configuration() -> bool:
+    variables = [
+        "MATRIX_MODE",
+        "DATABASE_URL",
+        "API_KEY",
+        "LOG_LEVEL",
+        "ZION_ENDPOINT"
+    ]
+    for key in variables:
+        if not os.getenv(key):
+            return False
+        if key == "MATRIX_MODE":
+            if (os.getenv(key) != "development" and
+                    os.getenv(key) != "production"):
+                return False
+        if key == "DATABASE_URL":
+            db_url = os.getenv("DATABASE_URL")
+            if not db_url or "://" not in db_url:
+                return False
+    return True
+
+
 def main() -> None:
     print("ORACLE STATUS: Reading the Matrix...")
     load_dotenv()
@@ -16,13 +57,18 @@ def main() -> None:
     print("\nConfiguration loaded:")
     print(f"Mode: {mode}")
     print(
-        f"Database: {'Connected to local instance' if database else 'Not connected'}")
+        "Database: "
+        f"{'Connected to local instance' if database else 'Not connected'}")
     print(f"API Access: {'Authenticated' if api else 'Not authenticated'}")
     print(f"Log Level: {log}")
     print(f"Zion Network: {'Online' if zion else 'Offline'}")
 
     print("\nEnvironment security check:")
-    
+    if check_harcoded_secrets(sys.argv[0]):
+        print("[OK] No hardcoded secrets detected")
+    else:
+        print("[FAIL] hardcoded secrets detected in source code")
+    if env_configuration()
 
 
 if __name__ == "__main__":
