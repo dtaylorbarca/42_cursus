0c909a0a822bfc5b94c86416040971c7d0a2c7b0
Dylan Taylor Barca <dtaylorbarca@gmail.com>
2026-05-23 17:59:08 +0200

    python_module_04

--- Milestone_2/Python_Piscina/Python_Module_04/ex0/ft_ancient_text.py
+++ Milestone_2/Python_Piscina/Python_Module_04/ex0/ft_ancient_text.py
@@ -12,25 +12,17 @@ def main() -> None:
         text: typing.IO = open(sys.argv[1])
         print("---\n")
         content = text.read()
-        index: int = 0
-        line_count: int = 0
+        index = 0
         while index < (len(content) - 1):
-            print("[FRAGMENT", end= " ")
-            if line_count < 10:
-                print("00", end="")
-            elif line_count < 100:
-                print("0", end="")
-            print(f"{line_count}]", end=" ")
-            line_count += 1
             while index < len(content) and content[index] != "\n":
                 print(f"{content[index]}", end="")
                 index += 1
-            if index < (len(content) - 1):
+            if index < len(content):
                 print(f"{content[index]}", end="")
             index += 1
 
         text.close()
-        print("\n\n---")
+        print("\n---")
         print(f"File '{sys.argv[1]}' closed.")
     except PermissionError as e:
         print(f"Error opening file '{sys.argv[1]}': {e}")
