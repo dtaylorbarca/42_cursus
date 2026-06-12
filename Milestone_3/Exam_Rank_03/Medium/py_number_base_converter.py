107a6d761c82f9bcd880403a64c1224853f040bb
Dylan Taylor Barca <dtaylor-@student.42madrid.com>
2026-06-12 13:42:12 +0200

    Base_converted finished

--- Milestone_3/Exam_Rank_03/Medium/py_number_base_converter.py
+++ Milestone_3/Exam_Rank_03/Medium/py_number_base_converter.py
@@ -7,6 +7,8 @@ Return "ERROR" for invalid inputs (base, digits)
 
 
 def valid_letter(character: str, base: int) -> bool:
+    if not character.isupper() or not character.isalpha():
+        return False
     if len(character) != 1:
         return False
     if (ord(character) - ord("A")) % 26 >= (base - 10):
@@ -23,19 +25,28 @@ def number_base_converter(number: str, from_base: int, to_base: int) -> str:
         except ValueError:
             if not valid_letter(c, from_base):
                 return "ERROR"
+    if not (2 <= from_base <= 36) or not (2 <= to_base <= 36):
+        return "ERROR"
     digits = len(number) - 1
     base_10 = 0
     for c in number:
         try:
             base_10 += int(c) * (from_base ** digits)
         except ValueError:
-            base_10 += (ord(c) - ord("A")) % 26 + 10
+            base_10 += ((ord(c) - ord("A")) % 26 + 10) * (from_base ** digits)
         digits -= 1
     if to_base == 10:
-        return str(digits)
+        return str(base_10)
+    if base_10 == 0:
+        return "0"
+    converted = ""
     while base_10 > 0:
-        
-    
+        if base_10 % to_base > 9:
+            converted += chr((base_10 % to_base) - 10 + ord("A"))
+        else:
+            converted += str(base_10 % to_base)
+        base_10 = base_10 // to_base
+    return converted[::-1]
 
 
 if __name__ == "__main__":
